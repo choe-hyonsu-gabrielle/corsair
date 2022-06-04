c40f9d68fd20ade428729aa1b376ed96f8646294
choe-hyonsu-gabrielle <choehyonsu@gmail.com>
2022-06-04 16:01:08 +0900

    bigram search

--- corsair.py
+++ corsair.py
@@ -6,6 +6,10 @@ from tqdm import tqdm
 
 
 def to_pos_sequence(items):
+    """
+    :param items: list of dict objects from 'morpheme'
+    :return: str sequence of part-of-speech tagging
+    """
     word_id, word_buffer, sentence_buffer = 1, [], []
     try:
         for item in items:
@@ -22,11 +26,24 @@ def to_pos_sequence(items):
         return None
 
 
+def formatted_print(hit, key, target, left, center, right):
+    (filename, doc_id), w_idx = key[0].rsplit('/', maxsplit=1), key[1]
+    print(
+        f"{str(hit).ljust(6)}"
+        f"{filename.ljust(25)}{doc_id.ljust(23)}{str(w_idx).ljust(3)}"
+        '\033[96m ' + target.ljust(30) + '\033[0m'
+        f"{' '.join(left).rjust(40)} "
+        '\033[38;2;215;95;215m' + center + '\033[0m'
+        f" {' '.join(right).ljust(40)}"
+    )
+
+
 class Corpus:
     def __init__(self, root='corpora/*.json'):
         self.files = glob.glob(root)
         if isinstance(self.files, str):
             self.files = [self.files]
+        self.window_size = 5
 
         # data
         self.ids = list()
@@ -34,16 +51,18 @@ class Corpus:
         self.morphemes = defaultdict(str)
 
         # search index & statistics
-        self.raw_index = defaultdict(list)
-        self.pos_index = defaultdict(list)
-        self.stats = dict(sentences=0, eojeols=0, tokens=0, types=0)
+        self.raw_unigram_index = defaultdict(list)
+        self.raw_bigram_index = defaultdict(list)
+        self.pos_unigram_index = defaultdict(list)
+        self.pos_bigram_index = defaultdict(list)
+        self.stats = dict(sentences=0, words=0, tokens=0, types=0)
 
         # preprocessing
         self._load_corpus()
         self._indexing()
 
     def _load_corpus(self):
-        for file in tqdm(self.files, desc=f'Loading {len(self.files)} files...'):
+        for file in tqdm(self.files, desc=f'[Corsair] Loading {len(self.files)} files...'):
             with open(file, encoding='utf-8') as f:
                 filename = './' + file.rsplit(sep='\\')[-1]
                 for doc in json.load(f)['document']:
@@ -54,56 +73,70 @@ class Corpus:
                             self.texts[key] = snt['form']
                             self.morphemes[key] = to_pos_sequence(snt['morpheme'])
                             self.stats['sentences'] += 1
-                            self.stats['eojeols'] += len(snt['word'])
+                            self.stats['words'] += len(snt['word'])
                             self.stats['tokens'] += len(snt['morpheme'])
                             self.stats['types'] += len(set([_t['form'] + '/' + _t['label'] for _t in snt['morpheme']]))
 
     def _indexing(self):
-        for key in tqdm(self.ids, desc=f'Indexing {len(self.ids)} data...'):
+        for key in tqdm(self.ids, desc=f'[Corsair] Indexing {len(self.ids)} data...'):
             for w_i, w in enumerate(self.texts[key].split()):
-                self.raw_index[w].append((key, w_i))
+                self.raw_unigram_index[w].append((key, w_i))
+            for wh_i, w2 in enumerate(zip(self.texts[key].split(), self.texts[key].split()[1:])):
+                self.raw_bigram_index[' '.join(w2)].append((key, wh_i))
             for p_i, p in enumerate(self.morphemes[key].split()):
-                self.pos_index[p].append((key, p_i))
+                self.pos_unigram_index[p].append((key, p_i))
+            for ph_i, p2 in enumerate(zip(self.morphemes[key].split(), self.morphemes[key].split()[1:])):
+                self.pos_bigram_index[' '.join(p2)].append((key, ph_i))
         print(self.stats)
-        print('Size of raw_word_index:', len(self.raw_index))
-        print('Size of pos_word_index:', len(self.pos_index))
+        print(dict(raw_unigram_index=len(self.raw_unigram_index), raw_bigram_index=len(self.raw_bigram_index),
+                   pos_unigram_index=len(self.pos_unigram_index), pos_bigram_index=len(self.pos_bigram_index)))
 
-    def search(self, query, window_size=5):
+    def _ngram_search(self, query, index=None, space=None, xlsx_export=False):
+        ngram_len = len(query)
+        query = ' '.join(query)
         results = []
         counts = defaultdict(int)
-        for target in self.pos_index.keys():
-            if re.findall(query, target):
-                results.append((target, self.pos_index[target]))
-                counts[target] += len(self.pos_index[target])
+        for ngram in index.keys():
+            if re.findall(query, ngram):
+                results.append((ngram, index[ngram]))
+                counts[ngram] += len(index[ngram])
         hit = 1
         for target, indices in results:
-            for idx in indices:
-                snt_id, word_id = idx
-                text = self.morphemes[snt_id]
-                splits = text.split()
+            for key in indices:
+                snt_id, word_id = key
+                seq = space[snt_id]
+                splits = seq.split()
                 left = splits[:word_id]
-                if len(left) > window_size:
-                    left = ['⋯'] + left[-window_size:]
-                center = splits[word_id]
-                right = splits[word_id + 1:]
-                if len(right) > window_size:
-                    right = right[:window_size] + ['⋯']
-
-                # formatted printing
-                print(f"{str(hit).ljust(6)}"
-                      f"\t{str(idx).ljust(50)}"
-                      '\t\033[96m ' + target + '\033[0m\t'
-                      f"{' '.join(left).rjust(40)} "
-                      '\033[38;2;215;95;215m' + center + '\033[0m' +
-                      f" {' '.join(right).ljust(40)}")
-
+                center = ' '.join(splits[word_id:word_id + ngram_len])
+                right = splits[word_id + ngram_len:]
+                left_p, right_p = None, None
+                if self.window_size:
+                    left_p = ['⋯'] + left[-self.window_size:] if len(left) > self.window_size else left
+                    right_p = right[:self.window_size] + ['⋯'] if len(right) > self.window_size else right
+                formatted_print(hit, key, target, left_p, center, right_p)
                 hit += 1
-        print('Top-10 Freq:', sorted(counts.items(), key=lambda x: x[1], reverse=True)[:10])
-        print('Query:', '\033[31m' + query + '\033[0m')
+        print('Search query:', '\033[31m' + query + '\033[0m')
+        if counts:
+            print('Top-10 Freq:', sorted(counts.items(), key=lambda x: x[1], reverse=True)[:10])
+
+    def search(self, command):
+        action, *query = command.split(sep=' ')
+        if action == '-raw' and len(query) == 1:
+            self._ngram_search(query=query, index=self.raw_unigram_index, space=self.texts)
+        elif action == '-raw' and len(query) == 2:
+            self._ngram_search(query=query, index=self.raw_bigram_index, space=self.texts)
+        elif action == '-pos' and len(query) == 1:
+            self._ngram_search(query=query, index=self.pos_unigram_index, space=self.morphemes)
+        elif action == '-pos' and len(query) == 2:
+            self._ngram_search(query=query, index=self.pos_bigram_index, space=self.morphemes)
+        else:
+            print("Wrong command: [-raw/-rawx/-pos/-posx] [head expression] (tail expression)")
 
 
 if __name__ == '__main__':
     corpus = Corpus(root='corpora/*.json')
     for _ in range(10):
-        keyword = input('\n\n검색: ')
-        corpus.search(query=keyword)
+        keyword = input('\n[Corsair] command: ')
+        corpus.search(command=keyword)
+
+
